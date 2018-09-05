868a1fdc6fb0f1d5ee8fcdace8cc11967714775a
Dominic Miller <Dominic@app-order.com>
2018-09-05 11:10:01 -0700

    Initial Commit

---
+++ DMSegmentedControl/DMSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  DMSegmentedControl.h
+//  DMSegmentedControl
+//
+//  Created by Dominic on 9/5/18.
+//  Copyright © 2018 DominicMiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMSegmentedControl.
+FOUNDATION_EXPORT double DMSegmentedControlVersionNumber;
+
+//! Project version string for DMSegmentedControl.
+FOUNDATION_EXPORT const unsigned char DMSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMSegmentedControl/PublicHeader.h>
+
+
